451c4564380dfb1221f1868ffceb1373d270ebcb
Eustaquio122 <tiagofsousa@gmail.com>
2017-12-20 16:04:39 +0000

    Refactors input parsing to process decimal number

--- parser.hs
+++ parser.hs
@@ -3,7 +3,7 @@ module Parser where
 import Lexic
 
 parseInput :: String -> [Token]
-parseInput x = shunt (lex' x) [] []
+parseInput x = shunt (parse x) [] []
 
 shunt :: [Token] -> [Token] -> [Token] -> [Token]
 shunt [] [] zs             = zs
@@ -18,35 +18,42 @@ shunt (RBr:xs) (LBr:ys) zs = shunt xs ys zs
 shunt (RBr:xs) (y:ys) zs   = shunt (RBr:xs) ys (zs ++ [y])
 shunt _ _ _                = [Invalid]
 
-lex' :: String -> [Token]
-lex' ""       = []
-lex' (' ':xs) = lex' xs
-lex' xs       = [headToken] ++ lex' rest
-                 where headToken = getToken xs
-                       rest = drop (tokenLength headToken) xs
+parse :: String -> [Token]
+parse ""       = []
+parse (' ':xs) = parse xs
+parse xs       = [headToken] ++ parse rest
+                 where headToken = tokenize element
+                       element   = getElement xs
+                       rest      = drop (length element) xs
 
-getToken :: String -> Token
-getToken (x:xs)
-         | isOperator x = Op x
-         | x == '('     = LBr
-         | x == ')'     = RBr
-         | isDigit x    = Num $ readNumber (x:xs)
-         | otherwise    = Invalid
+tokenize :: String -> Token
+tokenize (x:xs)
+       | x == '('     = LBr
+       | x == ')'     = RBr
+       | isOperator x = Op x
+       | isDigit x    = Num (read (x:xs) :: Double)
+       | otherwise    = Invalid
 
-readNumber :: String -> Double
-readNumber xs = read (getNumber "" xs) :: Double
+getElement :: String -> String
+getElement (x:xs)
+         | isDigit x = getNumber "" (x:xs)
+         | otherwise = [x]
+
+-- readNumber :: String -> Double
+-- readNumber xs = read (getNumber "" xs) :: Double
 
 getNumber :: String -> String -> String
-getNumber xs (y:"")
-           | isDigit y = xs ++ [y]
-           | otherwise = xs
+getNumber xs ('.':y:ys)
+        | isDigit y = getNumber (xs ++ ['.', y]) ys
+        | otherwise = "§"
+getNumber (x:xs) "" = [x]
 getNumber xs (y:ys)
-           | isDigit y = getNumber (xs ++ [y]) ys
-           | otherwise = xs
+        | isDigit y = getNumber (xs ++ [y]) ys
+        | otherwise = xs
 
-tokenLength :: Token -> Int
-tokenLength (Num x)   = (length $ show x) - 2
-tokenLength _         = 1
+-- elementLength :: Token -> Int
+-- elementLength (Num x)   = (length $ show x)
+-- elementLength _         = 1
 
 isOperator x = elem x operators
 isDigit x    = elem x digits
