fd1ac1f254e32e6163498ec5c064959363602a20
Eustaquio122 <tiagofsousa@gmail.com>
2017-12-21 12:00:30 +0000

    Moves shunt function to calculator.hs

--- calculator.hs
+++ calculator.hs
@@ -5,12 +5,25 @@ import Parser
 import ErrorHandler
 
 calculate :: String -> Double
-calculate input = head $ postFixCalc (parseInput input) []
+calculate input = head $ postFixCalc (shunt (parse input) [] []) []
 
 postFixCalc :: [Token] -> [Double] -> [Double]
 postFixCalc [] xs = xs
 postFixCalc (Num x:xs) ys = postFixCalc xs (x:ys)
-postFixCalc (Op x:xs) (y1:y2:ys) = postFixCalc xs (flip (getOp x) y1 y2 : ys)
+postFixCalc (Op x:xs) (y1:y2:ys) = postFixCalc xs ((getOp x) y2 y1 : ys)
+
+shunt :: [Token] -> [Token] -> [Token] -> [Token]
+shunt [] [] zs             = zs
+shunt [] ys zs             = zs ++ ys
+shunt (Num x:xs) ys zs     = shunt xs ys (zs ++ [Num x])
+shunt (Op x:xs) [] zs      = shunt xs [Op x] zs
+shunt (Op x:xs) (y:ys) zs
+      | y > Op x           = shunt ([Op x] ++ xs) ys (zs ++ [y])
+      | otherwise          = shunt xs ([Op x] ++ (y:ys)) zs
+shunt (LBr:xs) ys zs       = shunt xs (LBr:ys) zs
+shunt (RBr:xs) (LBr:ys) zs = shunt xs ys zs
+shunt (RBr:xs) (y:ys) zs   = shunt (RBr:xs) ys (zs ++ [y])
+shunt x y z                = [Err "Parsing Error"]
 
 getOp :: (Floating a) => Char -> (a -> a -> a)
 getOp '+' = (+)
